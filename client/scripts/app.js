ca34a787c8b6a2124a58ec0d43313c7258d8aff0
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-07-20 14:35:31 -0700

    start the sprint

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,90 @@
 // YOUR CODE HERE:
+var message = {
+  username: 'dmitry',
+  text: 'asdasd',
+  roomname: '111'
+};
+
+var getMessage = function(callback) {
+	$.ajax({
+	  // This is the url you should use to communicate with the parse API server.
+	  url: 'https://api.parse.com/1/classes/chatterbox',
+	  type: 'GET',
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message received');
+	   	callback(data);
+	  },
+	  error: function (data) {
+	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	    console.error('chatterbox: Failed to receive message');
+	  }
+	});
+
+}
+
+
+var sendMessage = function() {
+	$.ajax({
+	  // This is the url you should use to communicate with the parse API server.
+	  url: 'https://api.parse.com/1/classes/chatterbox',
+	  type: 'POST',
+	  data: JSON.stringify(message),
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message sent');
+	  },
+	  error: function (data) {
+	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	    console.error('chatterbox: Failed to send message');
+  	}
+	});
+}
+
+var geTimeStamp = function(time){
+ var year = time.getFullYear();
+ var month = time.getMonth()+1;
+ var day = time.getDate();
+ var hour = time.getHours();
+ var minutes = time.getMinutes();
+ var seconds = time.getSeconds();
+ 
+ return '' + hour.toString() + 
+       ':' + minutes.toString() + 
+       ':' + seconds.toString();
+};
+
+var displayMessage = function(data) {
+	$('#messages').empty();
+	for(var i = 0; i<data.results.length; i++) {
+		var entry = data.results[i];
+		var $msg = $('<div class="msg"></div>');
+		$msg.text(': ' + filterXSS(entry.text));
+		var timeStamp = new Date(entry.createdAt);
+		$('<a class="timeago">'+filterXSS(geTimeStamp(timeStamp))+'</a>').appendTo($msg);
+		$('<a class="username">'+filterXSS(entry.username)+'</a>').prependTo($msg);
+		$('#messages').append($msg);
+		lastMessageIndex = i;
+	}
+
+}
+
+getMessage(displayMessage);
+setInterval(getMessage.bind(this, displayMessage, 500));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
